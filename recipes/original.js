7507c95d766d5084a6f3d9391fe873c740e55d55
guesswho461 <guesswho461@gmail.com>
2019-08-16 07:49:52 +0800

    update recipe

--- recipes/original.js
+++ recipes/original.js
@@ -1,1 +1,45 @@
-console.log('test123');
+const mqtt = require('mqtt');
+const delay = require('delay');
+const waitUntil = require('async-wait-until');
+
+const opt = {
+  port: 1883,
+  clientId: 'recipeOriginal'
+};
+const waitMotionDoneTimeout = 60000; //ms
+let motionStop = true;
+
+async function waitMotionDone() {
+  motionStop = false;
+  const result = await waitUntil(() => {
+    return motionStop;
+  }, waitMotionDoneTimeout);
+}
+
+console.log(opt.clientId + ' started');
+
+const client = mqtt.connect('mqtt://localhost', opt);
+
+client.on('connect', function () {
+  console.log('connect to broker OK');
+  client.subscribe('robot/status/stop');
+});
+
+client.on('message', function (topic, msg) {
+  console.log('topic ' + topic + ' - ' + msg);
+  if (topic === 'robot/status/stop') {
+    if (msg.toString() === 'true') {
+      motionStop = true;
+    } else {
+      motionStop = false;
+    }
+  }
+});
+
+(async () => {
+  await waitMotionDone();
+  client.publish('robot/cmd/jog/x', '50');
+  await waitMotionDone();
+  client.publish('robot/cmd/jog/x', '-50');
+})();
+
--- ui/server/server.js
+++ ui/server/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cors = require('cors')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const exec = require('child_process').exec;
 
-var port = 8081;
-var version = 'v1.0';
+const port = 8081;
+const version = 'v1.0';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
