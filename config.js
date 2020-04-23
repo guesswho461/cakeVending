75ecd40019256c0f9188795cc314e010cd288943
guesswho461 <guesswho461@gmail.com>
2020-04-23 11:45:12 +0800

    remove tokens in config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   fbMesChatBotToken:
-    "EAAHGWe0Bo1IBABMq7O3G0ZCpZCwjCyzFpVXRNShYUqeDjJzrrkRWSCgcKvEehoYYizuBbaZCmx0bndc3YGVbHq0tvrmewq81qRkS5YFf5kbBc16jtawQTsUj9a5jazDAF35fLoGy1jBTnyxgMsACP7e0hLlZC75rZBWxGORqb98ozy9cq6UynV6px2YOX088ZD",
+    "",
   token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjYWtlVmVuZGluZyIsIm5hbWUiOiJKYXNoIEhzdSJ9.DSYy7W5a6iLSre1cmRaWBEdxYGu81jzdwdWnbnqRr4c",
+    "",
   subject: "cakeVending",
   name: "Jash Hsu",
-  secret: "0919304983",
+  secret: "",
   backendVersion: "cakeVendingBackend v1.0",
   backendPort: 8081,
   mqttBrokerPort: 1883,
